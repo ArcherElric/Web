101d0a3ab62c582da0ea8994d41da98b1551a5c0
ArcherElric <547934492@qq.com>
2017-10-08 19:14:39 +0800

    继承tools

--- Web/tools/Tool.js
+++ Web/tools/Tool.js
@@ -20,6 +20,80 @@ function inherit(p) {
     return new f()
 }
 
+//特定场景下返回带补丁的extend()
+var extend = (function() {
+    //检查是否存在bug
+    for (var p in { toString: null}) {
+        return function extend(o) {
+            for (var i = 0; i < arguments.length; i++) {
+                var source = arguments[i]
+                for (var prop in source) o[prop] = source[prop]
+            }
+            return o
+        }
+    }
+    //如果代码执行到这里，说明for/in循环不会枚举测试对象的toString对象
+    //因此返回另一个版本的extend()函数，这个函数显示测试Object.prototype中的不可枚举属性
+    return function patched_extend(o) {
+        for (var i = 0; i < arguments.length; i++) {
+            var source = arguments[i]
+            //复制所有的可枚举属性
+            for (var prop in source) o[prop] = source[prop]
+            //现在检查特殊属性
+            for (var j = 0; j < prototypes.length; j++) {
+                prop = prototypes[j]
+                if (source.hasOwnProperty(prop)) o[prop] = source[prop]
+            }
+        }
+        return o
+    }
+    var prototypes = ["toString", "valueOf", "constructor", "hasOwnProperty", "isPrototypeOf",
+                        "propertyIsEnumberable", "toLocaleString",]
+} ())
+
+//用来枚举属性的对象工具函数
+//将p中的可枚举属性复制至o中，并返回o
+function merge(o, p) {
+    for (prop in p) {
+        if (o.hasOwnProperty[prop]) continue
+        o[prop] = p[prop]
+    }
+    return o
+}
+//如果o中的属性在p中没有同名的属性，则从o中删除该属性，并返回o
+function restrict(o, p) {
+    for (prop in o) {
+        if (! (prop in p)) delete o[prop]
+    }
+    return o
+}
+//如果o中的属性在p中存在同名属性，则中o中删除，并返回o
+function subtract(o, p) {
+    for (prop in p) {
+        delete o[prop]
+    }
+    return o
+}
+//返回一个同时拥有o和p属性的对象，o和p同名属性使用p属性
+function union(o, p) {
+    return extend(extend({},o), p)
+}
+//返回一个o和p同时拥有的属性的新对象
+function intersection(o, p) {
+    return restrict(extend({}, o), p)
+}
+//返回一个数组，包含o中可枚举的自有属性的名字
+function keys(o) {
+    if (typeof o !== "object") throw TypeError()
+    var result = []
+    for (var prop in o) {
+        if (o.hasOwnProperty(prop)) {
+            result.push(prop)
+        }
+    }
+    return result
+}
+
 //扩展类型的功能
 Function.prototype.method = function(name, func) {
     if (!this.prototype[name]) {
